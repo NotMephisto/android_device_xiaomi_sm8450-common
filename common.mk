ca61e414a8afba2808303dcbcaf2e0fa3c85a4bf
Mephisto <38382271+NotMephisto@users.noreply.github.com>
2026-02-12 13:04:13 +0300

    Add Dolby caller

--- common.mk
+++ common.mk
@@ -103,6 +103,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+# Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl:64
